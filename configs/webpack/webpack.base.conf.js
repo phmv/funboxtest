d50b2f12dbd37c8691aef089b8c43d259b669046
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2022-06-23 00:19:27 +0300

    Babel options deleted, now config works which is adding polyfills

--- configs/webpack/webpack.base.conf.js
+++ configs/webpack/webpack.base.conf.js
@@ -28,9 +28,6 @@ module.exports = {
         exclude: "/node_modules/",
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
         },
       },
       {
